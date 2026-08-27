6184d09a7174815ff9d6774843a31b79c4bd3b48
Lexi <alexisbennett.8507@gmail.com>
2026-08-27 15:22:20 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 "use strict";
 
 console.log("Classic Cards initialized.");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("pointerdown", () => {
+        button.classList.add("pressed");
+    });
+
+    button.addEventListener("pointerup", () => {
+        button.classList.remove("pressed");
+    });
+
+    button.addEventListener("pointercancel", () => {
+        button.classList.remove("pressed");
+    });
+
+    button.addEventListener("pointerleave", () => {
+        button.classList.remove("pressed");
+    });
+});
