0a2b0e128e5caa1c5afeabb94000c6c054973746
Lexi <alexisbennett.8507@gmail.com>
2026-08-27 11:36:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+"use strict";
 
+const CACHE_NAME = "classic-cards-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./css/style.css",
+    "./js/app.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
