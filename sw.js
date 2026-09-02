35b71e8d0109c3ddadc7501f105ba1c82f2750ca
Lexi <alexisbennett.8507@gmail.com>
2026-09-02 17:52:11 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,85 @@
 "use strict";
 
-const CACHE_NAME = "classic-cards-v1";
+const CACHE_NAME = "classic-cards-v2";
 
 const APP_FILES = [
     "./",
     "./index.html",
-    "./css/style.css",
-    "./js/app.js",
+    "./css/style.css?v=4",
+    "./js/app.js?v=6",
     "./manifest.json"
 ];
 
+
+/* =========================
+   Install
+   ========================= */
+
 self.addEventListener("install", event => {
+
     event.waitUntil(
+
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(APP_FILES))
+
     );
+
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+
+/* =========================
+   Activate
+   ========================= */
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys()
+            .then(cacheNames => {
+
+                return Promise.all(
+
+                    cacheNames
+                        .filter(
+                            cacheName =>
+                                cacheName !== CACHE_NAME
+                        )
+                        .map(
+                            cacheName =>
+                                caches.delete(cacheName)
+                        )
+
+                );
+
+            })
+
     );
+
+    self.clients.claim();
+});
+
+
+/* =========================
+   Fetch
+   ========================= */
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request);
+
+            })
+
+    );
+
 });
